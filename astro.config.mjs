6bdb483a45f6e7100e995d1a3fb709bc193591dc
Rebecca Black <rebecca@performantsoftware.com>
2024-01-22 16:00:48 -0500

    added react, tailwind, tina, mdx, sitemap, netlify integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [mdx(), tailwind(), sitemap(), react()]
+});
