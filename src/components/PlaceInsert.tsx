e932c7d573b1070fe623b0abcc18d3b3a59b70f8
Rebecca Black <rebecca@performantsoftware.com>
2024-01-24 20:58:16 -0500

    aesthetic tweaks to placeholder component for place inserts

--- src/components/PlaceInsert.tsx
+++ src/components/PlaceInsert.tsx
@@ -5,7 +5,7 @@ import { isModalOpen, currentPlace } from "../modalStore";
 const PlaceInsert = (props: any) => {
     return (
         <div className="flex flex-col gap-y-2 place-content-center mx-auto my-8">
-            <div className="bg-indigo-400 h-32 w-32 flex place-content-center mx-auto cursor-pointer place-tile" data-uuid={props.place.uuid}>
+            <div className="bg-indigo-400 h-32 w-32 flex place-content-center mx-auto cursor-pointer place-tile p-4 rounded-lg text-sm" data-uuid={props.place.uuid}>
                 { props.place.title }
             </div>
             <div className="text-center text-lg">{props.title}</div>
