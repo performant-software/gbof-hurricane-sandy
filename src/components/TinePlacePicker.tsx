7033a4e0b900ccaff563b43d4b6d16ca42c83b72
Rebecca Black <rebecca.ann.black@gmail.com>
2024-02-22 23:53:15 -0500

    updated place picker in tina

--- src/components/TinePlacePicker.tsx
+++ src/components/TinePlacePicker.tsx
@@ -1,6 +1,7 @@
-import { Listbox, Transition } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useState } from "react"
 import { wrapFieldsWithMeta } from "tinacms";
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 
 interface CustomTinaFieldProps {
@@ -12,54 +13,79 @@ interface CustomTinaFieldProps {
 const TinaPlacePicker = wrapFieldsWithMeta((props: CustomTinaFieldProps) => {
 
   const [places, setPlaces] = useState<any>();
+  const [query, setQuery] = useState('');
+  const [filteredPlaces, setFilteredPlaces] = useState<any>();
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch('https://core-data-cloud-staging-2c51db0617a5.herokuapp.com/core_data/public/places?project_ids=24').then(response => response.json())
-        .then(response => JSON.stringify(response));
+        .then(async response => {
+          const results = response.metadata.count;
+          const fullData = await fetch(`https://core-data-cloud-staging-2c51db0617a5.herokuapp.com/core_data/public/places?project_ids=24&per_page=${results}`).then(response => response.json()).then(response => JSON.stringify(response));
+          return fullData});
       setPlaces(JSON.parse(data));
     }
     fetchData();
   }, []);
 
+  useEffect(() => {
+    places && setFilteredPlaces(query === '' ? places.features : places.features.filter((place) => place.properties.title.toLowerCase().includes(query.toLowerCase())));
+  }, [places, query]);
+
   return (
-    <Listbox value={props.input.value} onChange={props.input.onChange}>
-      <div className="relative mt-1 z-[9999]">
-        <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-white hover:bg-indigo-500/20 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <span className="block truncate">{places && places.features.find((place) => place.properties.uuid == props.input.value.uuid)?.properties.title || 'Select a place'}</span>
-        </Listbox.Button>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-            {places && places.features.map((place) => (
-              <Listbox.Option
-                key={place.properties.uuid}
-                className={({ active }) =>
-                  `relative cursor-pointer select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                  }`
-                }
-                value={{ uuid: place.properties.uuid, title: place.properties.title }}
-              >
-                {({ selected }) => (
-                  <>
-                    <span
-                      className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                        }`}
-                    >
-                      {place.properties.title}
-                    </span>
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
+    <Combobox value={props.input.value} onChange={props.input.onChange}>
+      <div className="z-[9999] relative">
+        <div className="relative mt-2">
+          <Combobox.Input
+            className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            onChange={(event) => setQuery(event.target.value)}
+            displayValue={(place) => place?.properties?.title}
+            placeholder="Type to Search"
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </Combobox.Button>
+
+          {filteredPlaces && filteredPlaces.length > 0 && (
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredPlaces.map((place) => (
+                <Combobox.Option
+                  key={place.properties.uuid}
+                  value={{ uuid: place.properties.uuid, title: place.properties.title }}
+                  className={({ active }) =>
+                    classNames(
+                      'relative cursor-default select-none py-2 pl-3 pr-9',
+                      active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                    )
+                  }
+                >
+                  {({ active, selected }) => (
+                    <>
+                      <span className={classNames('block truncate', selected && 'font-semibold')}>{place.properties.title}</span>
+
+                      {selected && (
+                        <span
+                          className={classNames(
+                            'absolute inset-y-0 right-0 flex items-center pr-4',
+                            active ? 'text-white' : 'text-indigo-600'
+                          )}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          )}
+        </div>
       </div>
-    </Listbox>
+    </Combobox>
   )
 });
 
