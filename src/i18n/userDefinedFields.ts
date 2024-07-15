b79a53b4fd91fe61bda78f151cf2960c06a8b14c
Rebecca Black <rebecca.ann.black@gmail.com>
2024-07-15 10:43:30 -0400

    updating tina schema

--- src/i18n/userDefinedFields.ts
+++ src/i18n/userDefinedFields.ts
@@ -3,7 +3,11 @@ import * as m from "../paraglide/messages.js";
 const userDefinedFields = {
 	"d3ca7e8c-9b03-4899-bcd9-114bab530b11": {
 		caption: m.t_d3ca7e8c9b034899bcd9114bab530b11(),
-		tinaLabel: "Place"
+		tinaLabel: "Places"
+	},
+	"2cee5956-5eff-4ed3-9dac-497a48986dcf": {
+		caption: m.t_2cee59565eff4ed39dac497a48986dcf(),
+		tinaLabel: "Places -> Description"
 	},
 	"b475eeea-ac1f-4276-be52-97b4fa67c5c4": {
 		caption: m.t_b475eeeaac1f4276be5297b4fa67c5c4(),
@@ -13,9 +17,25 @@ const userDefinedFields = {
 		caption: m.t_434eabb2034a4b058024151fc9f5e1f5(),
 		tinaLabel: "Images -> Notes"
 	},
+	"645fe270-b4b1-4d05-b5e0-06be58e998a8": {
+		caption: m.t_645fe270b4b14d05b5e006be58e998a8(),
+		tinaLabel: "Community Organizations"
+	},
+	"7fb2cfed-e5f4-4188-a9be-089a97afc204": {
+		caption: m.t_7fb2cfede5f44188a9be089a97afc204(),
+		tinaLabel: "Events"
+	},
 	"debf1773-58c3-4a0e-ae71-3d4fdd081f3a": {
 		caption: m.t_debf177358c34a0eae713d4fdd081f3a(),
-		tinaLabel: "Place -> Images"
+		tinaLabel: "Places -> Images"
+	},
+	"c4a62e61-79f5-4b11-a160-a00e289a9103": {
+		caption: m.t_c4a62e6179f54b11a160a00e289a9103(),
+		tinaLabel: "Events -> Location"
+	},
+	"f511a538-75ac-4f17-abf8-e54a9fe6df5b": {
+		caption: m.t_f511a53875ac4f17abf8e54a9fe6df5b(),
+		tinaLabel: "Places -> Organizations who operated in this location"
 	}
 }
 
